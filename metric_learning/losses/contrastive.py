006736bf338ac9e6b4ede694dfd88d3724b0cd5c
Frans de Boer <frans-de-boer@hotmail.com>
2022-10-14 14:22:39 +0200

    formatted code

--- metric_learning/losses/contrastive.py
+++ metric_learning/losses/contrastive.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class ContrastiveLoss(nn.Module):
     def __init__(self, distance: nn.Module, margin: float = 1.0, positive_weight: float = 0.5) -> None:
         super().__init__()
@@ -11,10 +12,11 @@ class ContrastiveLoss(nn.Module):
     def forward(self, x: torch.Tensor, y: torch.Tensor, x_labels: torch.Tensor, y_labels: torch.Tensor) -> torch.Tensor:
         equal: torch.Tensor = x_labels == y_labels
         distances: torch.Tensor = self.distance(x, y)
-        
+
         # Calculate the positive loss $(D_W)^2$
         positive_loss = equal * self.positive_weight * (distances).square()
         # Calculate the negative loss $max(0, m - D_W)^2$
-        negative_loss = (~equal) * (1 - self.positive_weight) * (self.margin - distances).clamp(0).square()
+        negative_loss = (~equal) * (1 - self.positive_weight) * \
+            (self.margin - distances).clamp(0).square()
 
         return (positive_loss + negative_loss).sum()
--- metric_learning/main.py
+++ metric_learning/main.py
@@ -1,10 +1,7 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
 
 from distances import EuclideanDistance
 from losses import ContrastiveLoss
@@ -17,31 +14,20 @@ def main():
     batch_size = 4
 
     trainset = torchvision.datasets.MNIST(root='./data', train=True,
-                                            download=True, transform=transform)
+                                          download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.MNIST(root='./data', train=False,
-                                           download=True, transform=transform)
+                                         download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=2)
 
-    data = torch.tensor(  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    data = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     labels = torch.tensor([0, 1, 0, 1, 0, 1, 0, 1, 0, 1])
     print(ContrastiveMiner(dimensionality=1)(data, labels))
 
     return
-
-    classes = ('plane', 'car', 'bird', 'cat',
-               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-    # functions to show an image
-
-    def imshow(img):
-        img = img / 2 + 0.5     # unnormalize
-        npimg = img.numpy()
-        plt.imshow(np.transpose(npimg, (1, 2, 0)))
-        plt.show()
 
     # get some random training images
     dataiter = iter(trainloader)
