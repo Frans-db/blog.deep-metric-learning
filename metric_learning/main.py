b75c826527fda914d5b12344e50a28d8e7f2954f
Frans de Boer <frans-de-boer@hotmail.com>
2022-10-14 14:16:54 +0200

    contrastive miner

--- metric_learning/main.py
+++ metric_learning/main.py
@@ -8,21 +8,24 @@ import torch.nn.functional as F
 
 
 def main():
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    transform = transforms.ToTensor()
 
     batch_size = 4
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    trainset = torchvision.datasets.MNIST(root='./data', train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    testset = torchvision.datasets.MNIST(root='./data', train=False,
                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=2)
+
+    for i in testset:
+        print(i)
+
+    return
 
     classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
--- metric_learning/miners/contrastive.py
+++ metric_learning/miners/contrastive.py
@@ -1,2 +1,14 @@
-class ContrastiveMiner:
-    pass
+import torch
+import torch.nn as nn
+from typing import Tuple
+
+
+class ContrastiveMiner(nn.Module):
+    def forward(self, embeddings: torch.Tensor, labels: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        # Transform data from
+        # 1 2 3 4 5 6 7 8 9 10
+        # to
+        # (1, 2), (3, 4), (5, 6), (7, 8), (9, 10)
+        embeddings = embeddings.reshape(2, embeddings.shape[0] // 2, 2)
+        labels = labels.reshape(2, labels.shape[0] // 2)
+        return embeddings, labels
