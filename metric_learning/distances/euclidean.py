357d00414e80f0b1e2c082fa38e78800114a2d46
Frans de Boer <frans-de-boer@hotmail.com>
2022-10-14 14:07:33 +0200

    euclidean distance

--- metric_learning/distances/euclidean.py
+++ metric_learning/distances/euclidean.py
@@ -1,2 +1,10 @@
-class EuclideanDistance:
-    pass
+import torch
+import torch.nn as nn
+
+
+class EuclideanDistance(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        return (x - y).square().sum(dim=-1).sqrt()
