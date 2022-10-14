0b2d0f54f0ebe4128425b70e358d0256c47275d4
Frans de Boer <frans-de-boer@hotmail.com>
2022-10-14 14:33:43 +0200

    basic network

--- metric_learning/networks/lecun.py
+++ metric_learning/networks/lecun.py
@@ -1,2 +1,23 @@
-class LecunNetwork:
-    pass
+# Network from Dimensionality Reduction by Learning an Invariant Mapping
+# http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+
+class LecunConvolutionalNetwork(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 15, 6)
+        self.pool1 = nn.AvgPool2d(3)
+        self.conv2 = nn.Conv2d(15, 30, 9)
+        self.fc1 = nn.Linear(30, 2)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        return x
